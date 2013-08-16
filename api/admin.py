e77ba4bfc5f3416e501d64d508410b00b3db62eb
David Ogutu <davidogutu@gmail.com>
2013-08-16 02:14:23 -0700

    added admin page

---
+++ api/admin.py
@@ -0,0 +1,7 @@
+from api.models import Cafeteria, Attribute, Meal, Serving
+from django.contrib import admin
+
+admin.site.register(Cafeteria)
+admin.site.register(Attribute)
+admin.site.register(Meal)
+admin.site.register(Serving)
